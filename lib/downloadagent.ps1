00d7814b4d3fa1dda2d5640d5adc850c2cb2ad5a
Bob <bob+git@vulpin.com>
2019-09-07 04:33:49 +1000

    Create dir

--- lib/downloadagent.ps1
+++ lib/downloadagent.ps1
@@ -2,6 +2,7 @@ $baseurl = 'https://github.com/benpye/wsl-ssh-pageant/releases/latest/download/'
 $files = @('wsl-ssh-pageant-amd64.exe', 'LICENSE');
 
 $agentroot = Join-Path $PSScriptRoot 'agent';
+mkdir -Force $agentroot;
 cd $agentroot;
 #wget $url -OutFile wsl-ssh-pageant-amd64.exe
 foreach ($file in $files) {
