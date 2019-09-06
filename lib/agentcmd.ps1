1ab9eb4534bcdc9cfaec6052c25f7c7690bd2e92
Bob <bob+git@vulpin.com>
2019-09-07 04:24:10 +1000

    Detect and handle failure to start agent

--- lib/agentcmd.ps1
+++ lib/agentcmd.ps1
@@ -1,6 +1,7 @@
 Import-Module $PSScriptRoot\agentmodule.psm1
 
 Start-Agent
+Assert-AgentRunning
 
 Write-Host "Agent started at SSH_AUTH_SOCK=$env:SSH_AUTH_SOCK"
 
--- lib/agentmodule.psm1
+++ lib/agentmodule.psm1
@@ -1,7 +1,31 @@
 function Start-Agent {
     $guid = New-Guid;
-    $sockname = "ssh-pageant-$guid";
+    $pipename = "ssh-pageant-$guid";
     $command = Join-Path $PSScriptRoot 'wsl-ssh-pageant-amd64.exe';
-    Start-Job {&$Using:command --winssh $Using:sockname} > $null;
-    $env:SSH_AUTH_SOCK = "\\.\pipe\$sockname";
+
+    if (!(Test-Path $command)) {
+        Write-Host -ForegroundColor Red "Could not find agent at $command. Have you run downloadagent?";
+        Read-Host -Prompt "Press Enter to exit";
+        exit;
+    }
+
+    $job = Start-Job {&$Using:command --winssh $Using:pipename};
+
+    $pipepath = "\\.\pipe\$pipename";
+
+    while (!(Test-Path $pipepath) -and $job.State -eq 'Running') {
+        sleep 0.1;
+        Receive-Job $job;
+    }
+    Receive-Job $job;
+
+    $env:SSH_AUTH_SOCK = $pipepath;
+}
+
+function Assert-AgentRunning {
+    if (!(Test-Path $env:SSH_AUTH_SOCK)) {
+        Write-Host -ForegroundColor Red "Failed to start agent";
+        Read-Host -Prompt "Press Enter to exit";
+        exit;
+    }
 }
